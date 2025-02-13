4f66d6104c85d2a7936c3bc25b56ff3654b644dd
AidanTrierweiler <atrierweiler@ithaca.edu>
2025-02-13 14:01:58 -0500

    when i installed dependeices some packages got updated. Also added routes in coursesupport.jsx to make demos in the test file work

--- src/main/js/CourseSupport.jsx
+++ src/main/js/CourseSupport.jsx
@@ -4,6 +4,12 @@ import { exampleCourseAttendance } from "../../test/js/examples/AttendanceCourse
 import AttendanceReportDisplay from "./AttendanceReportDisplay";
 import ClassroomDashboard from "./ClassroomDashboard"
 import CourseSupportHeader from "./CourseSupportHeader"
+import GroupBuilderDemo from '../../test/js/GroupBuilderDemo';
+import AttendanceCheckerDemo from '../../test/js/AttendanceCheckerDemo';
+import ClassroomDashboardDemo from '../../test/js/ClassroomDashboardDemo';
+import SingleStudentSelectorDemo from '../../test/js/SingleStudentSelectorDemo';
+import CourseSupportDemo from '../../test/js/CourseSupportDemo';
+import AttendanceReportDisplayDemo from '../../test/js/AttendanceReportDisplayDemo';
 
 const CourseSupport = (props) => {
 
@@ -15,6 +21,12 @@ const CourseSupport = (props) => {
                 <Route path="/" element={<CourseSupportHeader courseId={courseId} allCourseIds={[]} onCourseIdSelected={setCourseId} /> }>
                     <Route index element={<ClassroomDashboard courseId={courseId} studentNames={props.studentNames} /> }/>
                     <Route path="attendancereport" element={<AttendanceReportDisplay courseId={courseId} attendanceCourseReport={exampleCourseAttendance} />} />
+                    <Route path="group-builder-demo" element={<GroupBuilderDemo />} />
+                    <Route path="attendance-checker-demo" element={<AttendanceCheckerDemo />} />
+                    <Route path="classroom-dashboard-demo" element={<ClassroomDashboardDemo />} />
+                    <Route path="single-student-selector-demo" element={<SingleStudentSelectorDemo />} />
+                    <Route path="course-support-demo" element={<CourseSupportDemo />} />
+                    <Route path="attendance-report-demo" element={<AttendanceReportDisplayDemo />} />
                 </Route>
             </Routes>
         </BrowserRouter>
